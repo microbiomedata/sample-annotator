3a7d12a0eae119ac9d6d527ac5f5f097088aa6a7
cmungall <cjm@berkeleybop.org>
2021-10-05 18:18:55 -0700

    tidy

--- sample_annotator/clients/gold_client.py
+++ sample_annotator/clients/gold_client.py
@@ -25,6 +25,7 @@ class GoldClient:
 
     gold_key: USERPASS = None
     url: URL = "https://gold.jgi.doe.gov/rest/nmdc"
+    num_calls = 0
 
     def load_key(self, path: str) -> None:
         """
@@ -57,6 +58,7 @@ class GoldClient:
         logging.info(f'STATUS={results.status_code}')
         if results.status_code != 200:
             raise Exception(f'API call to {endpoint_url} failed, code={results.status_code}')
+        self.num_calls += 1
         return results.json()
 
     def fetch_biosamples_by_study(self, id: str) -> List[SampleDict]:
@@ -76,11 +78,57 @@ class GoldClient:
         :return:
         """
         id = self._normalize_id(id)
+        logging.info(f'Fetching study: {id}')
         results = self._call('studies', {'studyGoldId': id})
         study = results[0]
         if include_biosamples:
             study['biosamples'] = self.fetch_biosamples_by_study(id)
         return study
+
+    def fetch_study_by_biosample_id(self, id: str, include_biosamples=False) -> StudyDict:
+        """
+        given a biosample ID, fetch the containing study
+
+        :param id: E.g. Gb0011929
+        :param include_biosamples: if true, will also inject all biosamples for study (including the specified sample)
+        :return:
+        """
+        id = self._normalize_id(id)
+        logging.info(f'Fetching study: {id}')
+        results = self._call('studies', {'biosampleGoldId': id})
+        study = results[0]
+        if include_biosamples:
+            study['biosamples'] = self.fetch_biosamples_by_study(study['studyGoldId'])
+            logging.info(f'  Fetched biosamples for {id} == {len(study["biosamples"])}')
+        return study
+
+    def fetch_studies_by_biosample_ids(self, ids: List[str]) -> List[StudyDict]:
+        """
+        Given a list of biosample IDs, return the set of all studies that contain these
+
+        The study objects will also include lists of biosamples, and will include samples not in the original set
+
+        :param ids: E.g. Gb0011929, ...
+        :return:
+        """
+        logging.info(f'Fetching {len(ids)} samples')
+        biosample_to_study = {}
+        studies = []
+        n = 0
+        for biosample_id in ids:
+            n += 1
+            logging.debug(f'{biosample_id} is {n} of {len(ids)} // TOT: {len(biosample_to_study.keys())}')
+            if biosample_id in biosample_to_study:
+                logging.debug(f'Skipping {biosample_id} as already part of {biosample_to_study[biosample_id]}')
+            else:
+                study = self.fetch_study_by_biosample_id(biosample_id, include_biosamples=True)
+                logging.info(f"Fetched study for {biosample_id} SAMPLES= {len(study['biosamples'])}")
+                for biosample in study['biosamples']:
+                    sid = biosample['biosampleGoldId']
+                    logging.info(f'Adding {sid} to {study["studyGoldId"]}')
+                    biosample_to_study[sid] = study['studyGoldId']
+                studies.append(study)
+        return studies
 
     def fetch_studies(self, ids: List[str], **kwargs) -> List[StudyDict]:
         """
@@ -90,6 +138,7 @@ class GoldClient:
         :param kwargs:
         :return:
         """
+        logging.info(f'Fetching {len(ids)} studies')
         return [self.fetch_study(id, **kwargs) for id in ids]
 
     def fetch_studies_from_file(self, path: str, **kwargs) -> List[StudyDict]:
@@ -123,6 +172,8 @@ def main(verbose: int, quiet: bool):
 
 @main.command()
 @click.argument('idfile')
+@click.option('-d', '--directory',
+              help="if specified, calls in iterative fashion, making one file per study in dir")
 @click.option('--output', '-o',
               type=click.File(mode="w"),
               default=sys.stdout,
@@ -137,7 +188,7 @@ def main(verbose: int, quiet: bool):
 @click.option('--authentication-file', '-A',
               default='config/gold-key.txt',
               help="Path auth file. Contents should be user:pass")
-def fetch_studies(idfile, output: TextIO, output_format, authentication_file, **args):
+def fetch_studies(idfile, directory, output: TextIO, output_format, authentication_file, **args):
     """
     Fetch studies from gold
 
@@ -152,13 +203,48 @@ def fetch_studies(idfile, output: TextIO, output_format, authentication_file, **
     logging.info(f'Additional args: {args}')
     gc = GoldClient()
     gc.load_key(authentication_file)
-    studies = gc.fetch_studies_from_file(idfile, **args)
-    logging.info(f'Retrieved {len(studies)} studies')
-    with output as stream:
-        if output_format == 'yaml':
-            yaml.dump(studies, stream=stream, default_flow_style=False, sort_keys=False)
+    ids = []
+    with open(idfile) as file:
+        for line in file:
+            if line.startswith('Gs') or line.startswith('Gb'):
+                ids.append(line.strip())
+    if len(ids) == 0:
+        raise Exception(f'No ids in {idfile}')
+    study_ids = [id for id in ids if id.startswith('Gs')]
+    biosample_ids = [id for id in ids if id.startswith('Gb')]
+    id_type = None
+    if len(study_ids) > 0:
+        if len(biosample_ids) > 0:
+            raise Exception(f'Cannot mix and match study and sample IDs')
         else:
-            json.dump(studies, stream, indent=2, sort_keys=True)
+            id_type = 'study'
+    else:
+        id_type = 'biosample'
+    if directory:
+        if id_type == 'study':
+            for id in ids:
+                study = gc.fetch_study(id, **args)
+                logging.info(f'Retrieved {id}')
+                outpath = f'{directory}/{id}.{output_format}'
+                with open(outpath, 'w') as stream:
+                    if output_format == 'yaml':
+                        yaml.dump(study, stream=stream, default_flow_style=False, sort_keys=False)
+                    else:
+                        json.dump(study, stream, indent=2, sort_keys=True)
+        else:
+            raise Exception(f'directory + biosamples not supported')
+    else:
+        if id_type == 'study':
+            studies = gc.fetch_studies(ids, **args)
+        else:
+            studies = gc.fetch_studies_by_biosample_ids(ids)
+        logging.info(f'Retrieved {len(studies)} studies')
+        with output as stream:
+            if output_format == 'yaml':
+                yaml.dump(studies, stream=stream, default_flow_style=False, sort_keys=False)
+            else:
+                json.dump(studies, stream, indent=2, sort_keys=True)
+    logging.info(f'DONE. Calls = {gc.num_calls}')
 
 
 if __name__ == '__main__':
--- tests/test_gold.py
+++ tests/test_gold.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import logging
 import os
+import yaml
 from tests import MODEL_DIR, INPUT_DIR, OUTPUT_DIR
 
 from sample_annotator import SampleAnnotator
@@ -14,11 +15,15 @@ KEYPATH = os.path.join(INPUT_DIR, 'gold-key.txt')
 STUDY_LIST_PATH = os.path.join(INPUT_DIR, 'gold-studies-subset.tsv')
 
 TEST_STUDY_ID = 'Gs0144570' ## NEON
+TEST_BIOSAMPLE_IDS = ['Gb0255525', 'Gb0255899', 'Gb0255966',  ## in Gs0144570
+                      'Gb0011929']
+
+logging.basicConfig(level=logging.DEBUG)
 
 class TestGoldClient(unittest.TestCase):
     """elevation test."""
 
-    def test_get_samples(self):
+    def test_get_studies(self):
         gc = GoldClient()
         if os.path.exists(KEYPATH):
             gc.load_key(KEYPATH)
@@ -31,5 +36,19 @@ class TestGoldClient(unittest.TestCase):
             assert len(studies) > 0
             print(studies[0])
         else:
-            print(f'Skipping geolocation tests')
+            print(f'Skipping study tests')
+            print(f'To enable these, add your apikey to {KEYPATH}')
+
+    def test_get_biosamples(self):
+        gc = GoldClient()
+        if os.path.exists(KEYPATH):
+            gc.load_key(KEYPATH)
+            studies = gc.fetch_studies_by_biosample_ids(TEST_BIOSAMPLE_IDS)
+            print(len(studies))
+            print(studies[0])
+            print(studies[1])
+
+            assert len(studies) == 2
+        else:
+            print(f'Skipping sample tests')
             print(f'To enable these, add your apikey to {KEYPATH}')
