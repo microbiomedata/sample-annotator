b0d807108e1abb25cec82250b56586e054fb2dbc
Mark A. Miller <mamillerpa@gmail.com>
2021-10-21 16:29:05 -0400

    capitalization split off into sample_annotator.capitalizer

--- tests/test_capitalization.py
+++ tests/test_capitalization.py
@@ -1,18 +1,23 @@
 # -*- coding: utf-8 -*-
 import os
-from tests import MODEL_DIR, INPUT_DIR, OUTPUT_DIR
-import yaml
-import pdb
+import unittest
 
-from sample_annotator import SampleAnnotator, GeoEngine
+import yaml
+
+from sample_annotator import capitalizer
+# MODEL_DIR, INPUT_DIR, OUTPUT_DIR
+from tests import INPUT_DIR
 
 """Test the ability to capitalize a text slot."""
 
-import unittest
+"""Run as follows to get see test-time printouts:"""
+
+"""python -m pytest -sv  tests/test_capitalization.py"""
 
 # INPUT_DIR comes from __init__.py
 PWD = os.path.dirname(os.path.realpath(__file__))
 TEST_DATA = os.path.join(INPUT_DIR, 'test_sample_info.yaml')
+
 
 class TestCapitalization(unittest.TestCase):
     """capitalization test."""
@@ -21,11 +26,10 @@ class TestCapitalization(unittest.TestCase):
         with open(TEST_DATA) as stream:
             test_obj = yaml.load(stream, Loader=yaml.FullLoader)
         for t in test_obj.get('tests'):
-            print(t)
-            source = None
             desc = t.get('description', None)
-            print(desc)
-            pdb.set_trace()
+            # pdb.set_trace()
             if desc == 'capitalization test':
-                print(t)
-
+                current_input = t['sample']['text']
+                processed_input = capitalizer.capitalizer(current_input)
+                expected_output = t['output']['text']
+                assert processed_input == expected_output
