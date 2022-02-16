b5c2df844660d9984be9b2cc9651119a88c8192e
Mark Andrew Miller <MAM@lbl.gov>
2022-02-16 18:11:27 -0500

    take Message from report_model; uncomment utils/flatten.py

--- sample_annotator/__init__.py
+++ sample_annotator/__init__.py
@@ -1,10 +1,13 @@
 import os
 
-MAIN_MODEL_DIR = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'model')
-MAIN_SCHEMA_DIR = os.path.join(MAIN_MODEL_DIR, 'schema')
-MIXS_SCHEMA = os.path.join(MAIN_SCHEMA_DIR, 'mixs.json')
+MAIN_MODEL_DIR = os.path.join(os.path.abspath(os.path.dirname(__file__)), "model")
+MAIN_SCHEMA_DIR = os.path.join(MAIN_MODEL_DIR, "schema")
+MIXS_SCHEMA = os.path.join(MAIN_SCHEMA_DIR, "mixs.json")
 
 from .sample_annotator import SampleAnnotator
 from .geolocation.geotools import GeoEngine
+
 # , Message
 from .sample_annotator import AnnotationReport
+
+from report_model import Message
--- utils/flatten.py
+++ utils/flatten.py
@@ -1,13 +1,13 @@
-#!/usr/bin/env python3
+!/usr/bin/env python3
 
-# import yaml
-# import json
-# import sys
+import yaml
+import json
+import sys
 
-# with open(sys.argv[1]) as s:
-#     sset = yaml.safe_load(s)
-# samples = []
-# for s in sset:
-#     for k, v in s.copy().items():
-#         if 'has_raw_value'
-# print(json.dumps(obj, indent=4, sort_keys=True))
+with open(sys.argv[1]) as s:
+    sset = yaml.safe_load(s)
+samples = []
+for s in sset:
+    for k, v in s.copy().items():
+        if 'has_raw_value'
+print(json.dumps(obj, indent=4, sort_keys=True))
