15cf083e948075cf639faa26fa8c23356db1edce
Sujay Patil <sujaysanjeev.patil@gmail.com>
2022-04-21 02:57:26 -0700

    add gold: prefix to study_set attributes

--- sample_annotator/clients/nmdc/gold_client_wrapper.py
+++ sample_annotator/clients/nmdc/gold_client_wrapper.py
@@ -146,10 +146,10 @@ class GoldNMDC(GoldClient):
 
         self.nmdc_db.study_set.append(
             nmdc.Study(
-                id=study_data["studyGoldId"],
+                id="gold:" + study_data["studyGoldId"],
                 description=study_data["description"],
                 title=study_data["studyName"],
-                GOLD_study_identifiers=study_data["studyGoldId"],
+                GOLD_study_identifiers="gold:" + study_data["studyGoldId"],
                 principal_investigator=nmdc.PersonValue(
                     has_raw_value=pi_dict["name"],
                     name=pi_dict["name"],
