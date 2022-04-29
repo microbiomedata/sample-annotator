b4e81220d64f0cde7d2214dd2c0361cb43456f07
Jeanice Angelica Santosa <jeaniceangelica@berkeley.edu>
2022-04-29 10:18:49 -0700

    env_broad with edge cases

--- sample_annotator/non_edge.py
+++ sample_annotator/non_edge.py
@@ -3,6 +3,10 @@ import pandas as pd
 import numpy as np
 import re
 
+#biosample.db in this case is already in directory, 
+#replace biosample.db with the path of the database.
+#in this case, biosample.db contains the env_mapping table that I
+#have created.
 def to_csv():
     db = sqlite3.connect('biosample.db')
     cursor = db.cursor()
@@ -20,12 +24,20 @@ to_csv()
 map = pd.read_csv('new_env_mapping.csv')
 main = pd.read_csv('harmonized_wide_sel_envs.csv')
 
+#fixing with some edge cases
 main['env_broad_scale'] = main['env_broad_scale'].str.lower()
 main['env_broad_scale'] = main['env_broad_scale'].str.replace('envo:','', regex=True)
+main['env_broad_scale'] = main['env_broad_scale'].str.replace('env:','', regex=True)
+main['env_broad_scale'] = main ['env_broad_scale'].str.replace('\\(\d+\\)', '', regex=True)
+main['env_broad_scale'] = main ['env_broad_scale'].str.replace('\\[\d+\\]', '', regex=True)
+main['env_broad_scale'] = main ['env_broad_scale'].str.replace('marine biome ', 'marine biome', regex=True)
+main['env_broad_scale'] = main ['env_broad_scale'].str.replace('ocean biome ', 'ocean biome', regex=True)
+main['env_broad_scale'] = main['env_broad_scale'].str.replace('marine abyssal zone biome ', 'marine abyssal zone biome', regex=True)
+main['env_broad_scale'] = main['env_broad_scale'].str.replace(r"(\d+,?)(\s.+)", r"\1",regex=True)
 merge_table = main.merge(map, left_on = 'env_broad_scale', right_on = 'label', how='left')
 merge_table['broad_scale_fixed'] = merge_table['env_broad_scale'].astype(str) + ' ' + '['+merge_table['term_id'].astype(str) +']'
+merge_table['broad_scale_fixed'] = merge_table['broad_scale_fixed'].str.replace('(\\[nan\\])','', regex=True)
 
 #still need to change the 'nan[nan]' string values to NaN (np.nan) values 
 #not sure how
 merge_table['broad_scale_fixed'] = merge_table['broad_scale_fixed'].replace('nan[nan]',np.nan)
-merge_table['broad_scale_fixed'].value_counts()
