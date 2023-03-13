36c4d18725f9bd7dd3f8e9c64e8aa2c5b780efde
James Tessmer <james.tessmer@pnnl.gov>
2023-03-13 14:24:02 -0700

    Changing file structure a little bit to match current structure better

--- sample_annotator/clients/main.py
+++ sample_annotator/clients/main.py
@@ -1,8 +1,8 @@
 import sys
 from clients.submission_portal_client import submission_portal_client
 
-client = submission_portal_client('mappings.csv','no_path')
+client = submission_portal_client('../mappings.csv','no_path')
 map_dict = client.create_mapping_dict()
-df = client.read_data_file('example_data.json')
+df = client.read_data_file('../example_data.json')
 biosample_db = client.create_biosample_set(map_dict,df)
 client.dump_db(biosample_db,"output.json")
